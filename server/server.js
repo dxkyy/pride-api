1ee73c51c58cebfb872af551544b0018d9a97e99
dxkyy <adriandxkyy@gmail.com>
2022-06-16 12:25:17 +0200

    add redirect

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const apiRouter = require("./routes");
+const redirectRouter = require("./redirectroute.js");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", apiRouter);
+app.use("/", redirectRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
